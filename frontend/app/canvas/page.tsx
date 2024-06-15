666e6f5fe0b74418ee5f8690b23010d6d7037570
Anshul Gandhi <anshulgandhi2001@gmail.com>
2024-06-16 03:28:28 +0530

    Optimised renders

--- frontend/app/canvas/page.tsx
+++ frontend/app/canvas/page.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState } from 'react';
 import Toolbar from './(toolBar)/page';
+import {renderRect, renderCircle, drawLine, getDistance, areCollinearPoints} from '../canvas/render.utils'
 
 export default function Home() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -25,60 +26,63 @@ export default function Home() {
     coordinates: [],
   });
 
-  const drawLine = (
-    context: CanvasRenderingContext2D,
-    x1: number,
-    y1: number,
-    x2: number,
-    y2: number,
-    options?: { strokeColor?: string; strokeWidth?: number }
-  ): void => {
-    if (!context) return;
-    context.save();
-    context.lineJoin = 'round';
-    context.lineCap = 'round';
-    context.strokeStyle = options?.strokeColor || 'black';
-    context.lineWidth = options?.strokeWidth || 4;
+  const redrawFrame = (context: CanvasRenderingContext2D) => {
+    if (toolType === 'circle' || toolType === 'rect') {
+      context.clearRect(
+        0,
+        0,
+        canvasRef?.current?.width || 0,
+        canvasRef?.current?.height || 0
+      );
 
+      const start = performance.now();
+      drawExistingLines(context)
+      elements.forEach((element) => {
+        switch (element.type as Tool) {
+          case 'circle':
+            renderCircle(
+              context,
+              element.x1,
+              element.y1,
+              element.x2,
+              element.y2
+            );
+            break;
+          case 'rect':
+            renderRect(context, element.x1, element.y1, element.x2, element.y2);
+            break;
+
+          default:
+           break;
+        }
+      });
+
+      const end = performance.now();
+      console.log(end - start);
+    }
+  }
+  const drawExistingLines = (context: CanvasRenderingContext2D) => {
+    if(!context) return;
+    context.save();
     context.beginPath();
-    context.moveTo(x1, y1);
-    context.lineTo(x2, y2);
+    context.lineCap = 'round';
+    elements.filter(ele => ele.type === "line")?.forEach(element => {
+      // context.lineJoin = 'round';
+      context.strokeStyle ="black";
+      context.lineWidth = 8;
+      for(let i=0; i<element.coordinates.length; i++) {
+        context.moveTo(element.coordinates?.[i]?.[0],
+        element.coordinates?.[i]?.[1]);
+        context.lineTo(
+        element.coordinates?.[i + 1]?.[0],
+        element.coordinates?.[i + 1]?.[1])
+        }
+    })
     context.stroke();
     context.restore();
-  };
+  }
 
-  const renderCircle = (
-    context: CanvasRenderingContext2D,
-    x1: number,
-    y1: number,
-    x2: number,
-    y2: number
-  ) => {
-    if (!context) return;
-    const diameter = getDistance(x1, y1, x2, y2);
-    const originX = x1 + (x2-x1)/2
-    const originY = y1 + (y2-y1)/2
-    context.beginPath();
-    context.arc(originX, originY, diameter / 2, 0, 2 * Math.PI);
-    context.stroke();
-  };
-
-  const renderRect = (
-    context: CanvasRenderingContext2D,
-    x1: number,
-    y1: number,
-    x2: number,
-    y2: number
-  ) => {
-    if (!context) return;
-    context.beginPath();
-    context.strokeRect(x1, y1, x2 - x1, y2 - y1);
-    context.closePath();
-  };
-
-  const getDistance = (x1: number, y1: number, x2: number, y2: number) => {
-    return Math.sqrt(Math.pow(x1 - x2, 2)) + Math.sqrt(Math.pow(y1 - y2, 2));
-  };
+ 
 
   const handleTouchStart = (): void => {
     const context = canvasRef?.current?.getContext('2d');
@@ -122,52 +126,13 @@ export default function Home() {
       currCursorY: event.clientY,
     }));
 
+    
     if (!isDrawing) return;
-
     const context = canvasRef?.current?.getContext('2d');
     if (!context) return;
+    const lineElCoordinatesLength = lineElement.coordinates?.length;
 
-    if (toolType === 'circle' || toolType === 'rect') {
-      context.clearRect(
-        0,
-        0,
-        canvasRef?.current?.width || 0,
-        canvasRef?.current?.height || 0
-      );
-
-      const start = performance.now();
-      elements.forEach((element) => {
-        switch (element.type as Tool) {
-          case 'circle':
-            renderCircle(
-              context,
-              element.x1,
-              element.y1,
-              element.x2,
-              element.y2
-            );
-            break;
-          case 'rect':
-            renderRect(context, element.x1, element.y1, element.x2, element.y2);
-            break;
-
-          case 'line':
-            for (let i = 0; i < element?.coordinates?.length - 1; i++) {
-              drawLine(
-                context,
-                element.coordinates?.[i]?.[0],
-                element.coordinates?.[i]?.[1],
-                element.coordinates?.[i + 1]?.[0],
-                element.coordinates?.[i + 1]?.[1],
-                { strokeColor: 'black', strokeWidth: 8 }
-              );
-            }
-            break;
-        }
-      });
-      const end = performance.now();
-      console.log(end - start);
-    }
+    redrawFrame(context);
 
     switch (toolType) {
       case 'circle':
@@ -198,12 +163,13 @@ export default function Home() {
           cursorCoordinates.currCursorY,
           { strokeColor: 'black', strokeWidth: 8 }
         );
-        setLineElement((prev: any) => {
-          return {
-            type: toolType,
-            coordinates: [...prev?.coordinates, [event.clientX, event.clientY]],
-          };
-        });
+        if(!areCollinearPoints([cursorCoordinates.currCursorX, cursorCoordinates.currCursorY], [cursorCoordinates.prevCursorX, cursorCoordinates.prevCursorY], lineElement?.coordinates?.[lineElCoordinatesLength - 1]))
+          setLineElement((prev: any) => {
+            return {
+              type: toolType,
+              coordinates: [...prev?.coordinates, [event.clientX, event.clientY]],
+            };
+          });
 
         break;
     }
